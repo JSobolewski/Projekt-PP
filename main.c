a9c591c0d678b40078a214f3a60d5bb7fc76164d
Jakub Sobolewski <JS_Dev@live.com>
2017-12-07 00:03:26 +0100

    update

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct narzedzie {
     char nazwa[20];
@@ -17,6 +18,7 @@ typedef struct osoba {
 } os;
 
 void dodaj_narz(narz**);
+int dlugosc_listy(narz**);
 void edytuj_narz(narz**);
 void usun_narz(narz**);
 void wyswietl_wyp(narz**);
@@ -30,57 +32,60 @@ int main()
     narz *listaN = NULL;
     os *listaO = NULL;
     int wybor = -1;
-    printf("----- MENU -----\n\n");
-    printf("1. Dodaj narzedzie\n");
-    printf("2. Edytuj narzedzie\n");
-    printf("3. Usun narzedzie\n");
-    printf("4. Wyswietl wypozyczone narzedzia\n");
-    printf("5. Wyswietl niewypozyczone narzedzia\n\n");
-    printf("6. Dodaj osobe\n");
-    printf("7. Usun osobe\n");
-    printf("8. Wyswietl dane osoby na podstawie nazwiska(lub jego poczatku)\n\n");
-    printf("9. Zamknij program\n\n\n");
-    printf("Co wykonac? (1-9) ");
-    scanf("%d", &wybor);
-    while(wybor < 1 || wybor > 9) {
-        printf("\nNiepoprawny numer opcji, podaj jeszcze raz!\n\nCo wykonac? (1-9) ");
+    while(1) {
+        system("cls");
+        printf("----- MENU -----\n\n");
+        printf("1. Dodaj narzedzie\n");
+        printf("2. Edytuj narzedzie\n");
+        printf("3. Usun narzedzie\n");
+        printf("4. Wyswietl wypozyczone narzedzia\n");
+        printf("5. Wyswietl niewypozyczone narzedzia\n\n");
+        printf("6. Dodaj osobe\n");
+        printf("7. Usun osobe\n");
+        printf("8. Wyswietl dane osoby na podstawie nazwiska(lub jego poczatku)\n\n");
+        printf("9. Zamknij program\n\n\n");
+        printf("Co wykonac? (1-9) ");
         scanf("%d", &wybor);
-    }
-    switch(wybor) {
-        case 1:
-            printf("\n*** Dodawanie narzedzia ***\n\n");
-            dodaj_narz(&listaN);
-            break;
-        case 2:
-            printf("\n*** Edytowanie narzedzia ***\n\n");
-            edytuj_narz(&listaN);
-            break;
-        case 3:
-            printf("\n*** Usuwanie narzedzia ***\n\n");
-            usun_narz(&listaN);
-            break;
-        case 4:
-            printf("\n*** Wyswietlanie wypozyczonych narzedzi ***\n\n");
-            wyswietl_wyp(&listaN);
-            break;
-        case 5:
-            printf("\n*** Wyswietlanie niewypozyczonych narzedzi ***\n\n");
-            wyswietl_niewyp(&listaN);
-            break;
-        case 6:
-            printf("\n*** Dodawanie osoby ***\n\n");
-            dodaj_os(&listaO);
-            break;
-        case 7:
-            printf("\n*** Usuwanie osoby ***\n\n");
-            usun_os(&listaO);
-            break;
-        case 8:
-            printf("\n*** Wyswietlanie danych osoby na podstawie nazwiska(lub jego poczatku)***\n\n");
-            wyswietl_os(&listaO);
-            break;
-        case 9:
-            return 0;
+        while(wybor < 1 || wybor > 9) {
+            printf("\nNiepoprawny numer opcji, podaj jeszcze raz!\n\nCo wykonac? (1-9) ");
+            scanf("%d", &wybor);
+        }
+        switch(wybor) {
+            case 1:
+                printf("\n*** Dodawanie narzedzia ***\n\n");
+                dodaj_narz(&listaN);
+                break;
+            case 2:
+                printf("\n*** Edytowanie narzedzia ***\n\n");
+                edytuj_narz(&listaN);
+                break;
+            case 3:
+                printf("\n*** Usuwanie narzedzia ***\n\n");
+                usun_narz(&listaN);
+                break;
+            case 4:
+                printf("\n*** Wyswietlanie wypozyczonych narzedzi ***\n\n");
+                wyswietl_wyp(&listaN);
+                break;
+            case 5:
+                printf("\n*** Wyswietlanie niewypozyczonych narzedzi ***\n\n");
+                wyswietl_niewyp(&listaN);
+                break;
+            case 6:
+                printf("\n*** Dodawanie osoby ***\n\n");
+                dodaj_os(&listaO);
+                break;
+            case 7:
+                printf("\n*** Usuwanie osoby ***\n\n");
+                usun_os(&listaO);
+                break;
+            case 8:
+                printf("\n*** Wyswietlanie danych osoby na podstawie nazwiska(lub jego poczatku)***\n\n");
+                wyswietl_os(&listaO);
+                break;
+            case 9:
+                return 0;
+        }
     }
     return 0;
 }
@@ -103,16 +108,76 @@ void dodaj_narz(narz **lista) {
     if((*lista) == NULL) {
         *lista = pom;
     } else {
-        os *wsk = *lista;
+        narz *wsk = *lista;
         while(wsk->next != NULL) {
             wsk = wsk->next;
         }
         wsk->next = pom;
     }
+    free(pom);
+}
+
+int dlugosc_listy(narz **listaN) {
+    int i;
+    narz *wsk = *listaN;
+    if((*listaN) == NULL)
+        return 0;
+    else {
+        i = 1;
+        narz *wsk = *listaN;
+        while(wsk->next != NULL) {
+            i++;
+            wsk = wsk->next;
+        }
+    }
+    return i;
 }
 
 void edytuj_narz(narz **listaN) {
-
+    int n, i, dl_listy = dlugosc_listy(&listaN), wybor;
+    char nowaNazwa[20];
+    printf("Podaj numer narzedzia na liscie ktore chcesz edytowac: ");
+    scanf("%d", &n);
+    while(n < 0 || n > dl_listy) {
+        printf("Podaj poprawny numer narzedzia! Musi on sie miescic w przedziale <1; %d>\n", dl_listy);
+        scanf("%d", &n);
+    }
+    //if((*lista) == NULL) {
+        //printf("Lista pusta!");
+    //} else {
+        narz *wsk = *listaN;
+        i = 1;
+        while(wsk->next != NULL) {
+            i++;
+            wsk = wsk->next;
+        }
+        printf("Wybrales narzedzie nr %d na liscie. Obecne dane narzedzia to:\nNazwa: %s\nIlosc: %d\nData wypozyczenia: %d.%d.%d\nOkres wypozyczenia: %d dni\n\nCo chcesz w nim zmienic?\n1. Nazwe\n2. Ilosc\n3. Date wypozyczenia\n4. Okres wypozyczenia\n\nTwoj wybor (1-4): ", n, wsk->nazwa, wsk->ilosc, wsk->dzienWyp, wsk->miesiacWyp, wsk->rokWyp, wsk->okresWyp);
+        scanf("%d", &wybor);
+        while(wybor < 1 || wybor > 4) {
+            printf("Niepoprawny numer opcji, podaj jeszcze raz!\n\nTwoj wybor: ");
+            scanf("%d", &wybor);
+        }
+        switch(wybor) {
+            case 1:
+                printf("\nPodaj nowa nazwe: ");
+                gets(nowaNazwa);
+                strcpy(wsk->nazwa, nowaNazwa);
+                //wsk->nazwa = nowaNazwa;
+                break;
+            case 2:
+                printf("\nPodaj nowa ilosc: ");
+                scanf("%d", &wsk->ilosc);
+                break;
+            case 3:
+                printf("\nPodaj nowa date wypozyczenia(dzien miesiac rok): ");
+                scanf("%d %d %d", &wsk->dzienWyp, &wsk->miesiacWyp, &wsk->rokWyp);
+                break;
+            case 4:
+                printf("\nPodaj nowy okres wypozyczenia w dniach: ");
+                scanf("%d", &wsk->okresWyp);
+                break;
+        }
+    //}
 }
 
 void usun_narz(narz **listaN) {
